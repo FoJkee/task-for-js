6d3eb87a613ff7231c4cbb10f7c07c7328c8c733
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-09 18:16:16 +0300

    next task

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
-// Вычесть одно число из другого с помощью JavaScrip. Мы также можем вычесть одно число из другого. JavaScript использует -символ для вычитания //
+// ourStr would have a value of the string Hello, our name is freeCodeCamp, how are you?. Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well! //
 
-const difference = 45 - 33
+const myName = 'Viktor'
+const myStr = 'My name is ' + myName + 'I am well!'

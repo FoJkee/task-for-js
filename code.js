e2bf33c985277a7519dddcfc4b1cffe178118f49
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 17:38:05 +0300

    next task

--- code.js
+++ code.js
@@ -1,18 +1,21 @@
-/* Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+/* Use Bracket Notation to Find the Nth Character in a String
+You can also use bracket notation to get the character at other positions within a string.
 
-Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+Remember that computers start counting at 0, so the first character is actually the zeroth character.
 
-If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
-If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+Example:
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+secondLetterOfFirstName would have a value of the string d.
+
+Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+Hint: Try looking at the example above if you get stuck.
  */
 
-function hoopCount(n) {
-  for (let i = 0; i < n; i++) {
-    if (n >= 10) {
-      return 'Great, now move on to tricks'
-    } else {
-      return 'Keep at it until you get it'
-    }
-  }
-  return n
-}
+// Setup
+const lastName = 'Lovelace'
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2] // Change this line

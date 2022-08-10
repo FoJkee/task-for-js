22df144482e36e3fd2271351ddfdc5ae86bf8a2c
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 18:16:00 +0300

    next task

--- code.js
+++ code.js
@@ -1,21 +1,12 @@
-/* Use Bracket Notation to Find the Nth Character in a String
-You can also use bracket notation to get the character at other positions within a string.
+/* Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
-Remember that computers start counting at 0, so the first character is actually the zeroth character.
-
-Example:
-
-const firstName = "Ada";
-const secondLetterOfFirstName = firstName[1];
-secondLetterOfFirstName would have a value of the string d.
-
-Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
-
-Hint: Try looking at the example above if you get stuck.
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
  */
 
-// Setup
-const lastName = 'Lovelace'
-
-// Only change code below this line
-const thirdLetterOfLastName = lastName[2] // Change this line
+function paperwork(n, m) {
+  if (n < 0 || m < 0) {
+    return 0
+  } else {
+    return n * m
+  }
+}

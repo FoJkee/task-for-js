e92fafbad90e4716254b9adf6bb2b7c61435b9f2
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 17:32:28 +0300

    next task

--- code.js
+++ code.js
@@ -1,11 +1,18 @@
-/* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+/* Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
  */
 
-function even_or_odd(number) {
-  if (number % 2 == 0) {
-    return 'Even'
-  } else {
-    return 'Odd'
+function hoopCount(n) {
+  for (let i = 0; i < n; i++) {
+    if (n >= 10) {
+      return 'Great, now move on to tricks'
+    } else {
+      return 'Keep at it until you get it'
+    }
   }
-  return number
+  return n
 }

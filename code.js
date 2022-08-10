3ebf7bd6983723f3e97a87bace9df04b08db2b4e
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 18:37:27 +0300

    next task

--- code.js
+++ code.js
@@ -1,21 +1,20 @@
-/* Use Bracket Notation to Find the Nth-to-Last Character in a String
-You can use the same principle we just used to retrieve the last character in a string to retrieve the Nth-to-last character.
+/* Word Blanks
+You are provided sentences with some missing words, like nouns, verbs, adjectives and adverbs. You then fill in the missing pieces with words of your choice in a way that the completed sentence makes sense.
 
-For example, you can get the value of the third-to-last letter of the const firstName = "Augusta" string by using firstName[firstName.length - 3]
+Consider this sentence - It was really ____, and we ____ ourselves ____. This sentence has three missing pieces- an adjective, a verb and an adverb, and we can add words of our choice to complete it. We can then assign the completed sentence to a variable as follows:
 
-Example:
+const sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
+In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
 
-const firstName = "Augusta";
-const thirdToLastLetter = firstName[firstName.length - 3];
-thirdToLastLetter would have a value of the string s.
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
 
-Use bracket notation to find the second-to-last character in the lastName string.
-
-Hint: Try looking at the example above if you get stuck.
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
  */
 
-// Setup
-const lastName = 'Lovelace'
+const myNoun = 'dog'
+const myAdjective = 'big'
+const myVerb = 'ran'
+const myAdverb = 'quickly'
 
-// Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2] // Change this line
+const wordBlanks =
+  myAdjective + ', ' + myNoun + ', ' + myAdverb + ', ' + myVerb + '.'

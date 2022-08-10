a41921268715e2f85b712a0d8b0e3d8c4a5b3601
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 11:19:59 +0300

    next task

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
-/* In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+/* It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
  */
 
-function makeNegative(num) {
-  return num > 0 ? (num *= -1) : num
+function removeChar(str) {
+  return str.slice(1, -1)
 }

4ea3f3955518bb80b70f729f49940f31cd305f2a
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 22:18:43 +0300

    next task

--- code.js
+++ code.js
@@ -1,12 +1,7 @@
-/* You get an array of numbers, return the sum of all of the positives ones.
-
-Example [1,-4,7,12] => 1 + 7 + 12 = 20
-
-Note: if there is nothing to sum, the sum is default to 0.
-
+/* Complete the solution so that it reverses the string passed into it.
 
  */
 
-function positiveSum(arr) {
-  return arr.filter((x) => x > 0).reduce((sum, current) => sum + current, 0)
+function solution(str) {
+  return str.split('').reverse().join('')
 }

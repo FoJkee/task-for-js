07f79334bf73decb46d61bee4e39f2e142894de7
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-10 21:25:10 +0300

    next task

--- code.js
+++ code.js
@@ -1,11 +1,8 @@
-/* All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
-
-Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
-
-Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+/* Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 
  */
 
-function feast(beast, dish) {
-  return beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1)
+function smash(words) {
+  return words.join(' ')
 }

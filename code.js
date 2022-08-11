4cb2450f3f7c2b4d13fe79058d3cd070dd1e07b0
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 20:23:45 +0300

    next task

--- code.js
+++ code.js
@@ -1,20 +1,41 @@
-/* Understanding Boolean Values
-Another data type is the Boolean. Booleans may only be one of two values: true or false. They are basically little on-off switches, where true is on and false is off. These two states are mutually exclusive.
+/* Use Conditional Logic with If Statements
+if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
 
-Note: Boolean values are never written with quotes. The strings "true" and "false" are not Boolean and have no special meaning in JavaScript.
+When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
 
-Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+Pseudocode
 
+if (condition is true) {
+  statement is executed
+}
+Example
+
+function test (myCondition) {
+  if (myCondition) {
+    return "It was true";
+  }
+  return "It was false";
+}
+
+test(true);
+test(false);
+test(true) returns the string It was true, and test(false) returns the string It was false.
+
+When test is called with a value of true, the if statement evaluates myCondition to see if it is true or not. Since it is true, the function returns It was true. When we call test with a value of false, myCondition is not true and the statement in the curly braces is not executed and the function returns It was false.
+
+Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
 
 
 
 
  */
 
-function welcomeToBooleans() {
-  // Only change code below this line
-
-  return true // Change this line
-
-  // Only change code above this line
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return 'Yes, that was true'
+  } else {
+    return 'No, that was false'
+  }
 }
+trueOrFalse(true)
+trueOrFalse(false)

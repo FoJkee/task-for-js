b0c6122b39bb94eb49d22a8ee7b4fce07b284583
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 18:05:04 +0300

    next task

--- code.js
+++ code.js
@@ -1,18 +1,18 @@
-/* Write Reusable JavaScript with Functions
-In JavaScript, we can divide up our code into reusable parts called functions.
+/* Passing Values to Functions with Arguments
+Parameters are variables that act as placeholders for the values that are to be input to a function when it is called. When a function is defined, it is typically defined along with one or more parameters. The actual values that are input (or "passed") into a function when it is called are known as arguments.
 
-Here's an example of a function:
+Here is a function with two parameters, param1 and param2:
 
-function functionName() {
-  console.log("Hello World");
+function testFun(param1, param2) {
+  console.log(param1, param2);
 }
-You can call or invoke this function by using its name followed by parentheses, like this: functionName(); Each time the function is called it will print out the message Hello World on the dev console. All of the code between the curly braces will be executed every time the function is called.
+Then we can call testFun like this: testFun("Hello", "World");. We have passed two string arguments, Hello and World. Inside the function, param1 will equal the string Hello and param2 will equal the string World. Note that you could call testFun again with different arguments and the parameters would take on the value of the new arguments.
 
-Create a function called reusableFunction which prints the string Hi World to the dev console.
-Call the function.
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.
  */
 
-function reusableFunction() {
-  console.log('Hi World')
+function functionWithArgs(a, b) {
+  console.log(a + b)
 }
-reusableFunction()
+functionWithArgs(1, 2)

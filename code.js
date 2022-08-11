c7b253a52403221941810eb9499be3d471cc8423
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 10:39:01 +0300

    next task

--- code.js
+++ code.js
@@ -1,12 +1,6 @@
-/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
+/* Simple, remove the spaces from the string, then return the resultant string.
  */
 
-function boolToWord(bool) {
-  if (bool == true) {
-    return 'Yes'
-  } else {
-    return 'No'
-  }
-  return bool
+function noSpace(x) {
+  return x.split(' ').join('')
 }

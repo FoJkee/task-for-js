21a012d39b86c83811fda34db2c3cc4bb407abdb
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 19:01:04 +0300

    next task

--- code.js
+++ code.js
@@ -1,28 +1,34 @@
-/* Global vs. Local Scope in Functions
-It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+/* Understanding Undefined Value returned from a Function
+A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
 
-In this example:
+Example
 
-const someVar = "Hat";
+let sum = 0;
 
-function myFun() {
-  const someVar = "Head";
-  return someVar;
+function addSum(num) {
+  sum = sum + num;
 }
-The function myFun will return the string Head because the local version of the variable is present.
 
-Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+addSum(3);
+addSum is a function without a return statement. The function will change the global sum variable but the returned value of the function is undefined.
 
+Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
 
 
 
  */
 
-const outerWear = 'T-Shirt'
+let sum = 0
 
-function myOutfit() {
-  const outerWear = 'sweater'
-  return outerWear
+function addThree() {
+  sum = sum + 3
 }
+function addFive() {
+  sum = sum + 5
+}
+// Only change code below this line
 
-myOutfit()
+// Only change code above this line
+
+addThree()
+addFive()

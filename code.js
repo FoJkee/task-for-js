bc809e58dacacfc7eb7231e4a94a03de9b47e61e
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 12:42:48 +0300

    next task

--- code.js
+++ code.js
@@ -1,23 +1,21 @@
-/* Manipulate Arrays With push()
-An easy way to append data to the end of an array is via the push() function.
+/* Manipulate Arrays With pop()
+Another way to change the data in an array is with the .pop() function.
 
-.push() takes one or more parameters and "pushes" them onto the end of the array.
+.pop() is used to pop a value off of the end of an array. We can store this popped off value by assigning it to a variable. In other words, .pop() removes the last element from an array and returns that element.
 
-Examples:
+Any type of entry can be popped off of an array - numbers, strings, even nested arrays.
 
-const arr1 = [1, 2, 3];
-arr1.push(4);
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+The first console.log will display the value 6, and the second will display the value [1, 4].
 
-const arr2 = ["Stimpson", "J", "cat"];
-arr2.push(["happy", "joy"]);
-arr1 now has the value [1, 2, 3, 4] and arr2 has the value ["Stimpson", "J", "cat", ["happy", "joy"]].
-
-Push ["dog", 3] onto the end of the myArray variable.
+Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
  */
 
 const myArray = [
   ['John', 23],
   ['cat', 2],
 ]
-
-myArray.push(['dog', 3])
+const removedFromMyArray = myArray.pop()

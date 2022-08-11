1c80372b69326dcd74ca84d70e282c1e124d787b
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 12:23:07 +0300

    next task

--- code.js
+++ code.js
@@ -1,6 +1,23 @@
-/* We need a function that can transform a number (integer) into a string.
+/* Manipulate Arrays With push()
+An easy way to append data to the end of an array is via the push() function.
+
+.push() takes one or more parameters and "pushes" them onto the end of the array.
+
+Examples:
+
+const arr1 = [1, 2, 3];
+arr1.push(4);
+
+const arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+arr1 now has the value [1, 2, 3, 4] and arr2 has the value ["Stimpson", "J", "cat", ["happy", "joy"]].
+
+Push ["dog", 3] onto the end of the myArray variable.
  */
 
-function numberToString(num) {
-  return String(num)
-}
+const myArray = [
+  ['John', 23],
+  ['cat', 2],
+]
+
+myArray.push(['dog', 3])

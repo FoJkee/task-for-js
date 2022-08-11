5d75269140e1733f6c72350ec4ccace6fcfb0aa4
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-11 21:06:42 +0300

    next task

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
-/* Найдите наименьшее целое число в массиве
+/* Завершите функцию квадратной суммы, чтобы она возводила в квадрат каждое переданное ей число, а затем суммировала результаты.
+
+Например, for [1, 2, 2]это должно возвращаться , 9потому что 1^2 + 2^2 + 2^2 = 9.
  */
 
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-    return Math.min.apply(null, args)
-  }
+function squareSum(numbers) {
+  var result = 0
+  numbers.forEach(function (item) {
+    result += Math.pow(item, 2)
+  })
+  return result
 }

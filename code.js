eae35a7dba7773d67b59fc08890eb2d9ac942dae
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-12 13:07:39 +0300

    next task

--- code.js
+++ code.js
@@ -1,22 +1,20 @@
-/* Сравнение с оператором неравенства
-Оператор неравенства ( !=) противоположен оператору равенства. Это означает не равно и возвращает falseто, что вернулось бы к равенству, trueи наоборот . Как и оператор равенства, оператор неравенства преобразует типы данных значений при сравнении.
+/*Comparison with the Strict Inequality Operator
+The strict inequality operator (!==) is the logical opposite of the strict equality operator. It means "Strictly Not Equal" and returns false where strict equality would return true and vice versa. The strict inequality operator will not convert data types.
 
-Примеры
+Examples
 
-1 !=  2    // true
-1 != "1"   // false
-1 != '1'   // false
-1 != true  // false
-0 != false // false
-Добавьте оператор неравенства !=в ifоператор, чтобы функция возвращала строку , Not Equalкогда valона не эквивалентна 99.
+3 !==  3  // false
+3 !== '3' // true
+4 !==  3  // true
+Add the strict inequality operator to the if statement so the function will return the string Not Equal when val is not strictly equal to 17
  */
 
-function testNotEqual(val) {
-  if (val != 99) {
+function testStrictNotEqual(val) {
+  if (val !== 17) {
     // Change this line
     return 'Not Equal'
   }
   return 'Equal'
 }
 
-testNotEqual(10)
+testStrictNotEqual(10)

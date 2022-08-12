3f32d3977df9b5a8df926647446ed063a8ebdda2
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-12 15:38:37 +0300

    next task

--- code.js
+++ code.js
@@ -1,27 +1,48 @@
-/* Напишите цепочку операторов if/ else if, чтобы выполнить следующие условия:
+/* Golf Code
+In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
 
-num < 5- вернуться Tiny
-num < 10- вернуться Small
-num < 15- вернуться Medium
-num < 20- вернуться Large
-num >= 20- вернутьсяHuge
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
  */
 
-function testSize(num) {
-  if (num < 5) {
-    return 'Tiny'
-  } else if (num < 10) {
-    return 'Small'
-  } else if (num < 15) {
-    return 'Medium'
-  } else if (num < 20) {
-    return 'Large'
-  } else if (num >= 20) {
-    return 'Huge'
+const names = [
+  'Hole-in-one!',
+  'Eagle',
+  'Birdie',
+  'Par',
+  'Bogey',
+  'Double Bogey',
+  'Go Home!',
+]
+
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return 'Hole-in-one!'
+  } else if (strokes <= par - 2) {
+    return 'Eagle'
+  } else if (strokes == par - 1) {
+    return 'Birdie'
+  } else if (strokes == par) {
+    return 'Par'
+  } else if (strokes == par + 1) {
+    return 'Bogey'
+  } else if (strokes == par + 2) {
+    return 'Double Bogey'
+  } else if (strokes >= par + 3) {
+    return 'Go Home!'
   }
 
   return 'Change Me'
   // Only change code above this line
 }
 
-testSize(7)
+golfScore(5, 4)

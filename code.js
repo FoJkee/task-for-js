ee33afa3daecf692e5c313a65af5086544307e42
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-12 15:58:47 +0300

    next task

--- code.js
+++ code.js
@@ -1,48 +1,27 @@
-/* Golf Code
-In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
-
-Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
-
-Strokes	Return
-1	"Hole-in-one!"
-<= par - 2	"Eagle"
-par - 1	"Birdie"
-par	"Par"
-par + 1	"Bogey"
-par + 2	"Double Bogey"
->= par + 3	"Go Home!"
-par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
+/* Напишите оператор switch, который проверяет valи устанавливает answerследующие условия:
+1- alpha
+2- beta
+3- gamma
+4-delta
  */
 
-const names = [
-  'Hole-in-one!',
-  'Eagle',
-  'Birdie',
-  'Par',
-  'Bogey',
-  'Double Bogey',
-  'Go Home!',
-]
-
-function golfScore(par, strokes) {
-  if (strokes == 1) {
-    return 'Hole-in-one!'
-  } else if (strokes <= par - 2) {
-    return 'Eagle'
-  } else if (strokes == par - 1) {
-    return 'Birdie'
-  } else if (strokes == par) {
-    return 'Par'
-  } else if (strokes == par + 1) {
-    return 'Bogey'
-  } else if (strokes == par + 2) {
-    return 'Double Bogey'
-  } else if (strokes >= par + 3) {
-    return 'Go Home!'
+function caseInSwitch(val) {
+  let answer = ''
+  switch (val) {
+    case 1:
+      answer = 'alpha'
+      break
+    case 2:
+      answer = 'beta'
+      break
+    case 3:
+      answer = 'gamma'
+      break
+    case 4:
+      answer = 'delta'
+      break
   }
-
-  return 'Change Me'
-  // Only change code above this line
+  return answer
 }
 
-golfScore(5, 4)
+caseInSwitch(1)

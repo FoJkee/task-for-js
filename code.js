e8eec93ca8badb3d8ff7721a4755f9cd68370907
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-13 12:25:54 +0300

    next task

--- code.js
+++ code.js
@@ -1,56 +1,9 @@
-/* Замена цепочек If Else на Switch
-Если у вас есть много вариантов на выбор, switchнаписать оператор может быть проще, чем множество связанных операторов if/ . else ifСледующее:
-
-if (val === 1) {
-  answer = "a";
-} else if (val === 2) {
-  answer = "b";
-} else {
-  answer = "c";
-}
-можно заменить на:
-
-switch (val) {
-  case 1:
-    answer = "a";
-    break;
-  case 2:
-    answer = "b";
-    break;
-  default:
-    answer = "c";
-}
-Измените связанные операторы if/ else ifна switchоператор
+/* Исправьте функцию isLess, чтобы удалить if/elseоператоры.
  */
 
-function chainToSwitch(val) {
-  let answer = ''
-  // Only change code below this line
-
-  switch (val) {
-    case 'bob':
-      answer = 'Marley'
-      break
-    case 42:
-      answer = 'The Answer'
-      break
-    case 1:
-      answer = 'There is no #1'
-      break
-    case 99:
-      answer = 'Missed me by this much!'
-      break
-    case 7:
-      answer = 'Ate Nine'
-      break
-    case 'John':
-    case 156:
-      answer = ''
-      break
-  }
-
-  // Only change code above this line
-  return answer
+function isLess(a, b) {
+  return a === b || a < b
 }
 
-chainToSwitch(7)
+isLess(10, 15)
+isLess(15, 10)

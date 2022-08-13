405ed735487f3ae9bec526d6572f2d99705417fd
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-08-13 13:16:12 +0300

    next task

--- code.js
+++ code.js
@@ -1,9 +1,17 @@
-/* Исправьте функцию isLess, чтобы удалить if/elseоператоры.
+/* Измените функцию abTestтак, чтобы, если aили bменьше 0, функция немедленно завершалась со значением undefined.
+
+Подсказка
+Помните, что undefinedэто ключевое слово , а не строка.
  */
 
-function isLess(a, b) {
-  return a === b || a < b
+function abTest(a, b) {
+  if (a < 0) {
+    return undefined
+  } else if (b < 0) {
+    return undefined
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2))
 }
 
-isLess(10, 15)
-isLess(15, 10)
+abTest(2, 2)

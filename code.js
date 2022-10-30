a7f0217da13a21781a6fbd26ec51c59990f7443c
Viktor <Romanovsky0815@gmail.com>
2022-10-30 12:07:59 +0300

    task

--- code.js
+++ code.js
@@ -3,12 +3,12 @@
  */
 
 const myDog = {
-  name: 'Happy Coder',
-  legs: 4,
-  tails: 1,
-  friends: ['freeCodeCamp Campers'],
-  bark: 'woof',
-}
+    name: 'Happy Coder',
+    legs: 4,
+    tails: 1,
+    friends: ['freeCodeCamp Campers'],
+    bark: 'woof',
+};
 
 // Only change code below this line
-delete myDog.tails
+delete myDog.tails;

f1722fbe4600cd68ed6ceb267afc8b9c15e964e2
Viktor <Romanovsky0815@gmail.com>
2022-10-30 15:16:43 +0300

    task

--- code.js
+++ code.js
@@ -1,13 +1,16 @@
 /* 
-Функция raiseToPowerвозводит основание в степень. К сожалению, он не вызывается должным образом - исправьте код, чтобы значение powerбыло ожидаемым 8.
+Исправьте две ошибки индексации в следующей функции, чтобы все числа от 1 до 5 выводились на консоль.
 
  */
 
-function raiseToPower(b, e) {
-    return Math.pow(b, e);
+function countToFive() {
+    let firstFive = '12345';
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+        // Only change code above this line
+        console.log(firstFive[i]);
+    }
 }
 
-let base = 2;
-let exp = 3;
-let power = raiseToPower(base, exp);
-console.log(power);
+countToFive();

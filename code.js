9fd920eccba927a730a64aac705e2d385d80e5c7
Viktor <Romanovsky0815@gmail.com>
2022-10-30 13:07:42 +0300

    task

--- code.js
+++ code.js
@@ -1,13 +1,11 @@
 /* 
-Добавьте два console.log() оператора для проверки typeofкаждой из двух переменных sevenи threeв коде.
-
+Исправьте две орфографические ошибки в коде, чтобы netWorkingCapitalвычисления работали.
 
 
 
  */
 
-let seven = 7;
-let three = '3';
-console.log(seven + three);
-console.log(typeof seven);
-console.log(typeof three);
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);

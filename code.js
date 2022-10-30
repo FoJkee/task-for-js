43dd770dc8721582b97f02e357e003c93da14842
Viktor <Romanovsky0815@gmail.com>
2022-10-30 15:39:49 +0300

    task

--- code.js
+++ code.js
@@ -1,16 +1,23 @@
 /* 
-Исправьте две ошибки индексации в следующей функции, чтобы все числа от 1 до 5 выводились на консоль.
+Следующая функция предназначена для создания двумерного массива со mстроками и nстолбцами нулей. К сожалению, он не дает ожидаемого результата, потому что rowпеременная не инициализируется повторно (не устанавливается обратно в пустой массив) во внешнем цикле. Исправьте код, чтобы он возвращал правильный массив нулей 3x2, который выглядит как [[0, 0], [0, 0], [0, 0]].
 
  */
 
-function countToFive() {
-    let firstFive = '12345';
-    let len = firstFive.length;
-    // Only change code below this line
-    for (let i = 0; i < len; i++) {
-        // Only change code above this line
-        console.log(firstFive[i]);
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+
+    for (let i = 0; i < m; i++) {
+        let row = [];
+
+        for (let j = 0; j < n; j++) {
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
     }
+    return newArray;
 }
 
-countToFive();
+let matrix = zeroArray(3, 2);
+console.log(matrix);

9905490cacab66fac464cfce5db3e0caf25a1fb7
Viktor <Romanovsky0815@gmail.com>
2022-10-30 13:09:29 +0300

    task

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
 /* 
-Исправьте две орфографические ошибки в коде, чтобы netWorkingCapitalвычисления работали.
+Исправьте две парные ошибки в коде.
 
 
 
  */
 
-let receivables = 10;
-let payables = 8;
-let netWorkingCapital = receivables - payables;
-console.log(`Net working capital is: ${netWorkingCapital}`);
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => previous + current);
+console.log(`Sum of array values is: ${arraySum}`);

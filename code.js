2831a0b71ec1690146d63283e0c048b85185be58
Viktor <Romanovsky0815@gmail.com>
2022-10-30 15:13:12 +0300

    task

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 /* 
-Исправьте код, чтобы для переменной resultбыло установлено значение, возвращаемое при вызове функции getNine.
+Функция raiseToPowerвозводит основание в степень. К сожалению, он не вызывается должным образом - исправьте код, чтобы значение powerбыло ожидаемым 8.
 
  */
 
-function getNine() {
-    let x = 6;
-    let y = 3;
-    return x + y;
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
 }
 
-let result = getNine();
-console.log(result);
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);

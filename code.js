aa7459e3ef6a58702ea8fd4d8c47be270708c248
Viktor <Romanovsky0815@gmail.com>
2022-10-31 08:55:41 +0300

    task

--- code.js
+++ code.js
@@ -1,61 +1,13 @@
 /* 
-Изучите код в редакторе. Он использует метод, который имеет побочные эффекты в программе, вызывая неправильное поведение. Окончательный список открытых вкладок, хранящийся в finalTabs.tabs, должен быть таким, ['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium', 'new tab', 'Netflix', 'YouTube', 'Vine', 'GMail', 'Work mail', 'Docs', 'freeCodeCamp', 'new tab']но список, создаваемый кодом, немного отличается.
+ИВведите код функции incrementer, чтобы она возвращала значение глобальной переменной fixedValue, увеличенное на единицу.
 
-Измените Window.prototype.tabCloseтак, чтобы он удалял правильную вкладку.
+
 
 */
 
-// tabs is an array of titles of each site open within the window
-const Window = function (tabs) {
-    this.tabs = tabs; // We keep a record of the array inside the object
-};
+let fixedValue = 4;
 
-// When you join two windows into one window
-Window.prototype.join = function (otherWindow) {
-    this.tabs = this.tabs.concat(otherWindow.tabs);
-    return this;
-};
-
-// When you open a new tab at the end
-Window.prototype.tabOpen = function (tab) {
-    this.tabs.push('new tab'); // Let's open a new tab for now
-    return this;
-};
-
-// When you close a tab
-Window.prototype.tabClose = function (index) {
-    // Only change code below this line
-
-    const tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
-    const tabsAfterIndex = this.tabs.splice(1); // Get the tabs after the tab
-
-    this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
-
-    // Only change code above this line
-
-    return this;
-};
-
-// Let's create three browser windows
-const workWindow = new Window([
-    'GMail',
-    'Inbox',
-    'Work mail',
-    'Docs',
-    'freeCodeCamp',
-]); // Your mailbox, drive, and other work sites
-const socialWindow = new Window([
-    'FB',
-    'Gitter',
-    'Reddit',
-    'Twitter',
-    'Medium',
-]); // Social sites
-const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
-
-// Now perform the tab opening, closing, and other operations
-const finalTabs = socialWindow
-    .tabOpen() // Open a new tab for cat memes
-    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
-    .join(workWindow.tabClose(1).tabOpen());
-console.log(finalTabs.tabs);
+function incrementer() {
+    return fixedValue + 1;
+}
+incrementer();

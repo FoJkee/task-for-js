832945052543646a4ab552ea086b3fa7bfd8854d
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-02 15:55:42 +0300

    task

--- code.js
+++ code.js
@@ -1,11 +1,24 @@
 /*
+Given an array of integers.
 
-Implement a function which convert the given boolean value into its string representation.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
-Note: Only valid inputs will be given.
+If the input is an empty array or is null, return an empty array.
 
 */
 
-function booleanToString(b) {
-    return (b = String(b));
+function countPositivesSumNegatives(input) {
+    if (input === null || input.length < 1) {
+        return [];
+    }
+    let result = [0, 0];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+            result[0] += 1;
+        } else {
+            result[1] += input[i];
+        }
+    }
+    return result;
 }

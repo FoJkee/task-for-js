ed2433856adb4145023622fd4edecd781cf2858c
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-02 16:41:18 +0300

    task

--- code.js
+++ code.js
@@ -1,24 +1,10 @@
 /*
-Given an array of integers.
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
-
-If the input is an empty array or is null, return an empty array.
+Return your answer as a number.
 
 */
 
-function countPositivesSumNegatives(input) {
-    if (input === null || input.length < 1) {
-        return [];
-    }
-    let result = [0, 0];
-
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] > 0) {
-            result[0] += 1;
-        } else {
-            result[1] += input[i];
-        }
-    }
-    return result;
+function sumMix(x) {
+    return x.reduce((a, b) => +a + +b, 0);
 }

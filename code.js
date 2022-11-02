8de4cfd24453c56c66aeb2572aedb44aaf54aa33
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-02 20:31:54 +0300

    task

--- code.js
+++ code.js
@@ -3,9 +3,17 @@
 
 
 
-Попробуйте придать своей форме, которая теперь имеет idатрибут cat-photo-form, зеленый фон.
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 
 
 
 */
+
+function simpleMultiplication(number) {
+    if (number % 2 == 0) {
+        return number * 8;
+    } else {
+        return number * 9;
+    }
+}

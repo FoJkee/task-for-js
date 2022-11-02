87193bf65973cd7f825a10961b98d2b4b3d3fcf9
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-02 19:21:22 +0300

    task

--- code.js
+++ code.js
@@ -1,10 +1,4 @@
 /*
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-
-Return your answer as a number.
+Измените h2стиль вашего элемента, чтобы цвет его текста был красным.
 
 */
-
-function sumMix(x) {
-    return x.reduce((a, b) => +a + +b, 0);
-}

9e8816d38c6e4d0fc69d92d368b3bc7952a9d5a1
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-03 10:54:58 +0300

    task

--- code.js
+++ code.js
@@ -1,16 +1,10 @@
 /*
 
-You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
-If it is a square, return its area. If it is a rectangle, return its perimeter.
-
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 
 */
 
-const areaOrPerimeter = function (l, w) {
-    if (l == w) {
-        return l * w;
-    } else {
-        return (l + w) * 2;
-    }
-};
+function removeExclamationMarks(s) {
+    return s.replace(/[!]/gi, '');
+}

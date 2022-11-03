35eb2a9f261c0d822db101bb4ae66f735c506445
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-03 11:53:19 +0300

    task

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
 /*
 
-Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
 
 */
-
-function removeExclamationMarks(s) {
-    return s.replace(/[!]/gi, '');
-}
+String.prototype.toJadenCase = function () {
+    return this.split(' ')
+        .map((word) => word[0].toUpperCase() + word.substring(1))
+        .join(' ');
+};

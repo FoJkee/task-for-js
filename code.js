33e31a0160a705d6809d4cebed166ce55a33d7a1
Viktor <Romanovsky0815@gmail.com>
2022-11-08 20:23:59 +0300

    task

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 /*
-Чтобы мяч прыгал справа в непрерывном цикле, измените animation-iteration-countсвойство на infinite.
-
-
+Поддержите сердцебиение, добавив animation-iteration-countсвойство как для backкласса, так и для heartкласса и задав значение infinite. Селекторам heart:beforeи heart:afterне нужны никакие свойства анимации.
 
 
 

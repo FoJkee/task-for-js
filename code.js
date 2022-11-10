a8d8a7a75c0f4a10109d684bf3ecea37c0034f11
Viktor <Romanovsky0815@gmail.com>
2022-11-10 21:50:22 +0300

    task

--- code.js
+++ code.js
@@ -1,5 +1,16 @@
-const filteredList = watchList
-    .filter((film) => film.imdbRating >= 8.0)
-    .map((film) => ({ title: film['Title'], rating: film['imdbRating'] }));
+// The global variable
+const s = [23, 65, 98, 5];
 
-console.log(filteredList);
+Array.prototype.myFilter = function (callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i]) == true) {
+            newArray.push(this[i]);
+        }
+    }
+    return newArray;
+};
+
+const new_s = s.myFilter(function (item) {
+    return item % 2 === 1;
+});

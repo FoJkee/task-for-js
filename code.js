dd356bc6500fa48975e676d5daead3216a9fae8a
Viktor <Romanovsky0815@gmail.com>
2022-11-10 22:06:03 +0300

    task

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
-function sliceArray(anim, beginSlice, endSlice) {
-    return anim.slice(beginSlice, endSlice);
+function nonMutatingSplice(cities) {
+    // Only change code below this line
+    return cities.slice(0, 3);
+
+    // Only change code above this line
 }
 
-const inputAnim = ['Cat', 'Dog', 'Tiger', 'Zebra', 'Ant'];
-sliceArray(inputAnim, 1, 3);
+const inputCities = ['Chicago', 'Delhi', 'Islamabad', 'London', 'Berlin'];
+nonMutatingSplice(inputCities);

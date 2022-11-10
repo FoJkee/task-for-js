6d06bebcc9c17ab5abba40a11622df23a6fa06d6
Viktor <Romanovsky0815@gmail.com>
2022-11-10 15:43:27 +0300

    task

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 /*
-Добавьте свойство CSS align-selfв оба файла #box-1и #box-2. Дайте #box-1значение centerи дайте #box-2значение flex-end.
-
+Измените отображение div с containerклассом на grid.
 
 
 

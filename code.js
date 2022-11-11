4baa10823ab2e7455a76d8d5aef2cd87e0f1afe9
Viktor <Romanovsky0815@gmail.com>
2022-11-11 13:55:04 +0300

    task

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
-const squareList = (arr) => {
-    return arr
-        .filter((num) => num > 0 && num % parseInt(num) === 0)
-        .map((num) => Math.pow(num, 2));
-};
-
-const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
-console.log(squaredIntegers);
+function countBy(x, n) {
+    let z = [];
+    for (let i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}

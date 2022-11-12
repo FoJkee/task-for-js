50cabace66ce9cfdaea4c7ec849bf408f5c0fb13
Viktor <Romanovsky0815@gmail.com>
2022-11-12 14:33:07 +0300

    task

--- code.js
+++ code.js
@@ -1,23 +1,8 @@
-function getLength(arr) {
-    //return length of arr
-    return arr.length;
-}
-function getFirst(arr) {
-    //return the first element of arr
-    return arr[0];
-}
-function getLast(arr) {
-    //return the last element of arr
-    return arr[arr.length - 1];
-}
-function pushElement(arr) {
-    var el = 1;
-    arr.push(el);
-
-    return arr;
-}
-function popElement(arr) {
-    arr.pop();
-
-    return arr;
+function animal(obj) {
+    var str = {
+        name: 'dog',
+        legs: 4,
+        color: 'white',
+    };
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }

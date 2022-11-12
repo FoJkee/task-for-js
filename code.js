762d38aadcff0d43af69be80f5d5c50b00dc37ab
Viktor <Romanovsky0815@gmail.com>
2022-11-12 14:17:24 +0300

    task

--- code.js
+++ code.js
@@ -1,31 +1,23 @@
-var a1 = 'A',
-    a2 = 'a',
-    b1 = 'B',
-    b2 = 'b',
-    c1 = 'C',
-    c2 = 'c',
-    d1 = 'D',
-    d2 = 'd',
-    e1 = 'E',
-    e2 = 'e',
-    n1 = 'N',
-    n2 = 'n';
-function Dad() {
-    return d1 + a2 + d2;
+function getLength(arr) {
+    //return length of arr
+    return arr.length;
 }
-function Bee() {
-    return b1 + e2 + e2;
+function getFirst(arr) {
+    //return the first element of arr
+    return arr[0];
 }
-function banana() {
-    return b2 + a2 + n2 + a2 + n2 + a2;
+function getLast(arr) {
+    //return the last element of arr
+    return arr[arr.length - 1];
 }
+function pushElement(arr) {
+    var el = 1;
+    arr.push(el);
 
-function answer1() {
-    return 'no';
+    return arr;
 }
-function answer2() {
-    return 'no';
-}
-function answer3() {
-    return 'yes';
+function popElement(arr) {
+    arr.pop();
+
+    return arr;
 }

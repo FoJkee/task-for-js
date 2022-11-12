b0bbaf23902fe70257b6f1de16c7b888f1b0040b
Viktor <Romanovsky0815@gmail.com>
2022-11-12 18:28:26 +0300

    task

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
-function saleHotdogs(n) {
-    return n * (n < 5 ? 100 : n >= 10 ? 90 : 95);
+function howManydays(month) {
+    var days;
+    switch (month) {
+        case 2:
+            days = 28;
+            break;
+
+        case 4:
+            days = 30;
+            break;
+        case 6:
+            days = 30;
+            break;
+        case 9:
+            days = 30;
+            break;
+        case 11:
+            days = 30;
+            break;
+
+        default:
+            days = 31;
+    }
+    return days;
 }

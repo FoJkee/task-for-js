2178362d365667234dc89baac0a13941f90af5c8
Viktor <Romanovsky0815@gmail.com>
2022-11-12 19:09:05 +0300

    task

--- code.js
+++ code.js
@@ -1,25 +1,11 @@
-function howManydays(month) {
-    var days;
-    switch (month) {
-        case 2:
-            days = 28;
-            break;
-
-        case 4:
-            days = 30;
-            break;
-        case 6:
-            days = 30;
-            break;
-        case 9:
-            days = 30;
-            break;
-        case 11:
-            days = 30;
-            break;
-
-        default:
-            days = 31;
+function padIt(str, n) {
+    while (n > 0) {
+        if (n % 2) {
+            str = '*' + str;
+        } else {
+            str = str + '*';
+        }
+        n--;
     }
-    return days;
+    return str;
 }

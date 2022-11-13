c784f81f50d142d8c0264f1c4da8bb91e08d8b00
Viktor <Romanovsky0815@gmail.com>
2022-11-13 14:05:12 +0300

    task

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
-//afafaaf/
+function giveMeFive(obj) {
+    const mass = [];
+    for (let key in obj) {
+        if (key.length == 5) {
+            mass.push(key);
+        }
+        if (obj[key].length == 5) {
+            mass.push(obj[key]);
+        }
+    }
+
+    return mass;
+}

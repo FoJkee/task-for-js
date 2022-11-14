28d711ec27b485f9d6012bdec3b8a9d286eee2d1
Viktor <Romanovsky0815@gmail.com>
2022-11-14 12:18:30 +0300

    task

--- code.js
+++ code.js
@@ -1,13 +1,3 @@
-function giveMeFive(obj) {
-    const mass = [];
-    for (let key in obj) {
-        if (key.length == 5) {
-            mass.push(key);
-        }
-        if (obj[key].length == 5) {
-            mass.push(obj[key]);
-        }
-    }
-
-    return mass;
+function grow(x) {
+    return x.reduce((acc, num) => acc * num);
 }

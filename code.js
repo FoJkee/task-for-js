48de4761b135d4efc07c12a1201ab4cef1dbcc31
Viktor <Romanovsky0815@gmail.com>
2022-11-14 15:32:43 +0300

    task

--- code.js
+++ code.js
@@ -1,16 +1,3 @@
-function whatNumberIsIt(n) {
-    switch (n) {
-        case Number.MAX_VALUE:
-            return 'Input number is Number.MAX_VALUE';
-        case Number.MIN_VALUE:
-            return 'Input number is Number.MIN_VALUE';
-        case Number.POSITIVE_INFINITY:
-            return 'Input number is Number.POSITIVE_INFINITY';
-        case Number.NEGATIVE_INFINITY:
-            return 'Input number is Number.NEGATIVE_INFINITY';
-        case Number(n):
-            return 'Input number is ' + n;
-        default:
-            return 'Input number is Number.NaN';
-    }
+function colorOf(r, g, b) {
+    return `#${[r, g, b].map((i) => i.toString(16).padStart(2, 0)).join('')}`;
 }

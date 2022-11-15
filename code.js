7b913d9f98fd248052c3210974914b2ec77a71c7
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-15 21:46:57 +0300

    task

--- code.js
+++ code.js
@@ -1,17 +1,3 @@
-function bmi(weight, height) {
-    let p = weight / Math.pow(height, 2);
-    switch (true) {
-        case p <= 18.5:
-            return 'Underweight';
-            break;
-        case p > 18.5 && p <= 25:
-            return 'Normal';
-            break;
-        case p > 25 && p <= 30:
-            return 'Overweight';
-            break;
-        case p > 30:
-            return 'Obese';
-            break;
-    }
+function addBinary(a, b) {
+    return (Number(a) + Number(b)).toString(2);
 }

2ebf207301f0a0f9385c15f93c7b599fe9c2f227
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-16 21:39:44 +0300

    task

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
-const binaryArrayToNumber = (arr) => {
-    return Number(parseInt(arr.join(''), 2));
-};
+function isIsogram(str) {
+    str = str.toLowerCase();
+    var hash = {};
+    for (var i in str) {
+        if (hash[str[i]]) {
+            return false;
+        }
+        hash[str[i]] = true;
+    }
+    return true;
+}

ea7df48752b24c261eb0af203b85cc9a69e460f5
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-17 10:40:03 +0300

    task

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 function openOrSenior(data) {
-    const num = [];
+    let res = [];
     for (let i = 0; i < data.length; i++) {
-        num[i] = data[i][0] >= 55 && data[i][1] > 7 ? 'Senior' : 'Open';
+        res[i] = data[i][0] >= 55 && data[i][1] > 7 ? 'Senior' : 'Open';
     }
-    return num;
+    return res;
 }

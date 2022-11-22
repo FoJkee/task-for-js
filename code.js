396080ffd0b11733edfd4e2678ec52ebe7c2eecb
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-22 20:43:36 +0300

    task

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-function isVow(a) {
-    return a.map((x) =>
-        /[aeiou]/.test(String.fromCharCode(x)) ? String.fromCharCode(x) : x
-    );
+function removeUrlAnchor(url) {
+    return url.split('#')[0];
 }

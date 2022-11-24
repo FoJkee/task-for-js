88ab53e9b0036b791322b92073f089d80ab740bf
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-24 19:41:39 +0300

    task

--- code.js
+++ code.js
@@ -1,9 +1,3 @@
-function finalGrade(exam, projects) {
-    return 90 < exam || projects > 10
-        ? 100
-        : 75 < exam && projects >= 5
-        ? 90
-        : 50 < exam && projects >= 2
-        ? 75
-        : 0;
+function nameShuffler(str) {
+    return str.split(' ').reverse().join(' ');
 }

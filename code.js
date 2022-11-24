6ee0ca8564e1da0639289c1d1e2e07d4856eed5e
ViktorRomanovsky <Romanovsky0815@gmail.com>
2022-11-24 22:43:27 +0300

    task

--- code.js
+++ code.js
@@ -1,11 +1,3 @@
-function howMuchILoveYou(nbPetals) {
-    const num = [
-        'not at all',
-        'I love you',
-        'a little',
-        'a lot',
-        'passionately',
-        'madly',
-    ];
-    return num[nbPetals % 6];
+function square(num) {
+    return Math.pow(num, 2);
 }

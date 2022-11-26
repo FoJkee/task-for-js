15a5b173cb08ffc76a3d917db04b25f5fe743ef1
Viktor <Romanovsky0815@gmail.com>
2022-11-26 15:56:41 +0300

    task

--- code.js
+++ code.js
@@ -1,4 +1,17 @@
-function getChar(c) {
-    return String.fromCharCode(c);
-}
-//
+var humanYearsCatYearsDogYears = function (humanYears) {
+    let dogYears = 0;
+    let catYears = 0;
+
+    if (humanYears == 1) {
+        dogYears = 15;
+        catYears = 15;
+    } else if (humanYears == 2) {
+        dogYears = 24;
+        catYears = 24;
+    } else {
+        dogYears = 5 * (humanYears - 2) + 24;
+        catYears = 4 * (humanYears - 2) + 24;
+    }
+
+    return [humanYears, catYears, dogYears];
+};
